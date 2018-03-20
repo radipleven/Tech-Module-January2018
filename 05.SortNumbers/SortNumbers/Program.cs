3796891cc2fddac4f706cff50604c650d2596f3f
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:34:38 +0200

    Add files via upload

---
+++ 05.SortNumbers/SortNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            numbers.Sort();
+            Console.WriteLine(string.Join(" <= ", numbers));
+        }
+    }
+}
