4df07451163c92a926c16e94ac28cdc4dd83e8bb
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:54:16 +0200

    Add files via upload

---
+++ 01.Convert from Base-10 to Base-N/01.Convert from Base-10 to Base-N/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _01.Convert_from_Base_10_to_Base_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            int n = int.Parse(input[0]);
+            BigInteger number = new BigInteger();
+            number = BigInteger.Parse(input[1]);
+            BigInteger reminder = 0;
+            string result = string.Empty;
+            while (number>0)
+            {
+                reminder = number % n;
+                number = number / n;
+                result = reminder.ToString() + result;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
