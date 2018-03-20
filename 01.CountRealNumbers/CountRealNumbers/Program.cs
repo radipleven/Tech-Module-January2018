2f2a411f8020f511b5bf95643c22d73ed84209e4
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:15:12 +0200

    Add files via upload

---
+++ 01.CountRealNumbers/CountRealNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var num in numbers)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+                foreach (var neshto in counts.Keys)
+                {                   
+                        Console.WriteLine($"{num} -> {counts[num]}");                    
+                }
+            }
+        }
+    }
+}
+
