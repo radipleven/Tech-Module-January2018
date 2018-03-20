f092fea41789c25be6c997038f22a0cc642f8c39
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:45:11 +0200

    Add files via upload

---
+++ 03.TextFilter/03.TextFilter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace _03.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = Console.ReadLine().Split(' ');
+            string text = Console.ReadLine();
+            
+            
+            foreach (var banWord in banWords)
+            {
+                text = text.Replace(banWord, new string('*', banWord.Length));
+            }
+            Console.WriteLine(text);
+
+        }
+    }
+}
---
+++ 04.Palindromes/04.Palindromes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split(new char[] {'.',' ',',','!','?' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> toPrint = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (word == ReverseString(word))
+                {
+                    toPrint.Add(word);
+                }
+            }
+            toPrint.Sort();
+            List<string> unique = toPrint.Distinct().ToList();
+            Console.WriteLine(string.Join(", ",unique));
+
+        }
+        static string ReverseString(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
