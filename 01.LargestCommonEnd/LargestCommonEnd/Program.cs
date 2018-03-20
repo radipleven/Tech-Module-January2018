9f186a5dfa2f43402acc747317af9e5569761757
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:59:18 +0200

    Add files via upload

---
+++ 01.LargestCommonEnd/LargestCommonEnd/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words1 = Console.ReadLine().Split(' ').ToArray();
+            string[] words2 = Console.ReadLine().Split(' ').ToArray();
+
+            int leftCount = 0;
+            int rightCount = 0;
+
+            for (int i = 0; i < Math.Min(words1.Length, words2.Length); i++)
+            {
+                if (words1[i] == words2[i])
+                {
+                    leftCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            for (int i = 0; i < Math.Min(words1.Length, words2.Length) ; i++)
+            {
+                if (words1[words1.Length-1-i] == words2[words2.Length - 1 - i])
+                {
+                    rightCount++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            if (leftCount>rightCount )
+            {
+                Console.WriteLine(leftCount);
+            }
+            else
+            {
+                Console.WriteLine(rightCount);
+            }
+
+        }
+    }
+}
