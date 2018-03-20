593ef7f7a32b8d7e187e0a9be48cdf1a1cef4605
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:05:29 +0200

    Add files via upload

---
+++ 05.CompareCharArrays/CompareCharArrays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace CompareCharArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word1 = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            char[] word2 = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+            char[] smaller;
+
+            bool isFirst = false;
+
+            for (int i = 0; i < Math.Min(word1.Length, word2.Length); i++)
+            {
+                if (word1[i] < word2[i])
+                {
+                    isFirst = true;
+                    break;
+                }
+                else if (word1[i] > word2[i])
+                {
+                    isFirst = false;
+                    break;
+                }
+
+                if (i == Math.Min(word1.Length, word2.Length) - 1)
+                {
+                    if (word1.Length < word2.Length)
+                    {
+                        isFirst = true;
+                    }
+                    else
+                    {
+                        isFirst = false;
+                    }
+                }
+
+
+            }
+            if (isFirst)
+            {
+                Console.WriteLine(string.Join("",word1));
+                Console.WriteLine(string.Join("", word2));
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", word2));
+                Console.WriteLine(string.Join("", word1));
+            }
+
+        }
+
+    }
+}
