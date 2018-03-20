342b5ed52f46310ef19c75ce61d6ad05514360fc
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:41:51 +0200

    Add files via upload

---
+++ 01.ReverseString/01.ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(ReverseString(input));
+             
+
+
+        }
+        static string ReverseString (string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
