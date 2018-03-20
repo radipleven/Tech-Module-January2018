432090904e0a27fa09360b8413f6841d588bfb3f
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:20:42 +0200

    Add files via upload

---
+++ 04.Largest3Numbers/Largest3Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            if (numbers.Count<3)
+            {
+                Console.WriteLine(string.Join(" ",numbers.OrderByDescending(x => x)));
+            }
+            else
+            {
+                Console.Write(numbers.Max() + " ");
+                numbers.Remove(numbers.Max());
+                Console.Write(numbers.Max() + " ");
+                numbers.Remove(numbers.Max());
+                Console.WriteLine(numbers.Max());
+                
+            }
+
+        }
+    }
+}
