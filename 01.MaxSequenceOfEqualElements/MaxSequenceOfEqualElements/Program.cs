dbf782aa70f154e6e7b424c0d143f848e3ed3417
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:45:16 +0200

    Add files via upload

---
+++ 01.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var count = 1;
+            var bestCount = 0;
+            var num = 0;
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                    if (count > bestCount)
+                    {
+                        num = numbers[i];
+                        bestCount = count;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+
+
+            if (bestCount == 0)
+            {
+                Console.WriteLine($"{numbers[0]}");
+            }
+            else
+            {
+                for (int i = 0; i < bestCount; i++)
+                {
+                    Console.Write($"{num} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
