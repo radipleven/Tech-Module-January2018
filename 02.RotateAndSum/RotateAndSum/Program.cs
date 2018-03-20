28c8b3f71161a5d2972f9fb3fd63e2e0f76e181b
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:00:18 +0200

    Add files via upload

---
+++ 02.RotateAndSum/RotateAndSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace RotateAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int countOfRotations = int.Parse(Console.ReadLine());            
+            int[] sum = new int[numbers.Length];
+
+            for (int i = 0; i < countOfRotations; i++)
+            {
+                numbers = Rotate(numbers);
+                for (int j = 0; j < numbers.Length; j++)
+                { 
+                    sum[j] += numbers[j];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+
+         static int[] Rotate(int[] numbers)
+        {
+            int[] rotated = new int[numbers.Length];
+            int last = numbers[numbers.Length - 1];
+            for (int i = 1; i < rotated.Length; i++)
+            {
+                rotated[i] = numbers[i - 1];
+            }
+            rotated[0] = last;
+            return rotated;
+        }
+    }
+}
+
+
