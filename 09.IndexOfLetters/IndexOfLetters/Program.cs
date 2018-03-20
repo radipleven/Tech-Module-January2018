b62ffee6891866e3a9a87c41243e2a2b67f92496
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:08:18 +0200

    Add files via upload

---
+++ 09.IndexOfLetters/IndexOfLetters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace IndexOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //KRATKO RESHENIE
+            // string word = Console.ReadLine();
+            // foreach (var element in word)
+            //Console.WriteLine($"{element} -> {element - 'a'}");
+
+
+            string word = Console.ReadLine();
+            char[] letter = new char[word.Length];
+            var count = 0;
+            foreach (var element in word)
+            {
+                letter[count] = element;
+                count++;
+            }
+
+            for (int i = 0; i < letter.Length; i++)
+            {
+                count = 0;
+                for (char j = 'a'; j <= 'z'; j++)
+                {
+                    if (letter[i] == j)
+                    {
+                        Console.WriteLine($"{letter[i]} -> {count}");
+                    }
+                    count++;
+                }
+            }
+
+
+        }
+    }
+}
