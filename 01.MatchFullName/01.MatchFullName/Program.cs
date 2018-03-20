1d93aaf7295ecabe9ca18b03fc2840e901fa28ea
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:28:12 +0200

    Add files via upload

---
+++ 01.MatchFullName/01.MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\b[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+\b";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (var match in matches)
+            {
+                Console.Write(match + " ");
+            }
+            Console.WriteLine();
+                
+       
+        }
+    }
+}
