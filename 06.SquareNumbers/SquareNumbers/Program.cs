6a78191e7fc4b0e63bb823034e81f4a1f413dcde
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:37:12 +0200

    Add files via upload

---
+++ 06.SquareNumbers/SquareNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> squares = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (Math.Sqrt(numbers[i]) == (int)Math.Sqrt(numbers[i]))
+                {
+                    squares.Add(numbers[i]);
+                }
+            }
+            squares.Sort();squares.Reverse();
+            Console.WriteLine(string.Join(" ",squares));
+        }
+    }
+}
