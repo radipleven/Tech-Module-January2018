cad71c750e2f2dfd3a5599d749dd40807272c366
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:31:56 +0200

    Add files via upload

---
+++ 02.AppendLists/AppendLists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AppendLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+           List<string> separateLists = input.Split('|').ToList();
+           
+
+            separateLists.Reverse();
+            foreach (var text in separateLists)
+            {
+                var textArr = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var joined = string.Join(" ", textArr);
+                Console.Write(joined + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
