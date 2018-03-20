a508819c6a11298c2db92a47fa4ea071d4646d3e
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:37:14 +0200

    Add files via upload

---
+++ 01.ExtractEmails/01.ExtractEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\s{1}[0-9A-Za-z]+-*(\.)*(\w+-*)@(\w+(-\w+)*)(\.\w+)+\b(?<=\w)*";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
