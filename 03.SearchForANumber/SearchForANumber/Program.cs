9c4e980c06ff46828fa3004659a3cd50ccdb20a0
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:53:28 +0200

    Add files via upload

---
+++ 03.SearchForANumber/SearchForANumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SearchForANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();  //1 2 3 4 5 6 //numbers.Count = 6;
+            int[] manipulator = Console.ReadLine().Split().Select(int.Parse).ToArray(); //5 2 3
+           
+            numbers.RemoveRange(manipulator[0],numbers.Count-manipulator[0]);
+             for (int i = 0 ; i < manipulator[1]; i++)
+             {
+                 numbers.RemoveAt(0);
+             }
+           
+           
+            
+
+           if (numbers.Contains(manipulator[2]))
+           {
+               Console.WriteLine("YES!");
+           }else if (!numbers.Contains(manipulator[2]))
+           {
+               Console.WriteLine("NO!");
+           }
+
+        }
+    }
+}
