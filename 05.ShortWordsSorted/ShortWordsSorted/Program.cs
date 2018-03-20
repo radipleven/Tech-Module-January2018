2fa2849beb40845f67b0a653ac7b9eda51799394
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:21:38 +0200

    Add files via upload

---
+++ 05.ShortWordsSorted/ShortWordsSorted/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] separators = ".,:;()[]\"\'\\/!? ".ToCharArray();
+
+            string[] input = Console.ReadLine()
+                .ToLower()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ",input.Where(x => x.Length<5).Distinct().OrderBy(x=>x)));
+
+        }
+    }
+}
