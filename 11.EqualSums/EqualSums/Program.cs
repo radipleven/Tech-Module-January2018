70e527e94f80e49f00ba4c9ca637d70588a16863
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:09:50 +0200

    Add files via upload

---
+++ 11.EqualSums/EqualSums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var leftSum = 0;
+            var rightSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                leftSum += numbers[i];
+                rightSum = 0;
+                for (int j = i + 2; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+                if (leftSum == rightSum)
+                {
+                    int index = i + 1;
+                    Console.WriteLine(index);
+                    return;
+                }
+            }
+            rightSum = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                rightSum += numbers[i];
+            }
+            if (rightSum == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
