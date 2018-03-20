e6861411caaddeb0244792eedbb7ab1aea08ff13
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:07:36 +0200

    Add files via upload

---
+++ 08.MostFrequentNumber/MostFrequentNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            var countHelper = 0;
+            long digit = numbers[0];
+            for (long i = 0; i < numbers.Length - 1; i++)
+            {
+                var count = 0;
+                for (long j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > countHelper)
+                {
+                    digit = numbers[i];
+                    countHelper = count;
+                }
+            }
+            Console.WriteLine(digit);
+        }
+    }
+}
