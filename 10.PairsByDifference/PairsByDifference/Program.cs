a659ef0a99f9672d28785163e15c8ead63d19133
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:09:02 +0200

    Add files via upload

---
+++ 10.PairsByDifference/PairsByDifference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace PairsByDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int diff = int.Parse(Console.ReadLine());
+            var count = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] - numbers[j] == diff)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
