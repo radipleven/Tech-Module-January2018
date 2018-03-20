06c9c55feac3baa31af0e4b7e3050cf98e133690
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:55:00 +0200

    Add files via upload
    Partially correct solution.

---
+++ 07.BombNumbers/BombNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>(200);
+            numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int[] bomb = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (bomb[0]==numbers[i])
+                {
+                    if (true)
+                    {
+                        numbers.RemoveRange(i - bomb[1], bomb[1]);
+                    }
+                    numbers.RemoveRange(i, bomb[1]);
+                    numbers.Remove(bomb[0]);
+               }
+                
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
