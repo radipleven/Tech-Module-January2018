705421740174be888586587783754c995ced3aa7
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:03:55 +0200

    Add files via upload

---
+++ 03.FoldAndSum/FoldAndSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();        
+            int k = numbers.Length / 4;
+            int[] upper = new int[numbers.Length / 2];
+            int[] lower = new int[numbers.Length / 2];
+
+            for (int i = 0; i < 2*k; i++)
+            {
+                lower[i] = numbers[k + i];
+            }
+            for (int i = 0; i < k; i++)
+            {
+                upper[i] = numbers[k - 1 - i];
+                upper[k + i] = numbers[4 * k - 1 - i];
+            }
+            int[] sum = new int[2 * k];
+            for (int i = 0; i < 2*k; i++)
+            {
+                sum[i] = upper[i] + lower[i];
+            }
+            Console.WriteLine(string.Join(" ",sum));
+        }        
+    }
+}
