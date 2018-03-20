a1264662dfb942fe70d143cad4b95ede026eeb40
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:54:05 +0200

    Add files via upload

---
+++ 06.SumReversedNumbers/SumReversedNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumReversedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+           List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += ReverseInt(numbers[i]);
+            }
+
+            Console.WriteLine(sum);
+
+
+        }
+        static int ReverseInt(int num)
+        {
+            int result = 0;
+            while (num > 0)
+            {
+                result = result * 10 + num % 10;
+                num /= 10;
+            }
+            return result;
+        }
+    }
+}
