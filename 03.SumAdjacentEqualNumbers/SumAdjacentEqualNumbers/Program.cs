fd0222556a3816b15be4582a4cd71a990204334d
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 16:33:11 +0200

    Add files via upload

---
+++ 03.SumAdjacentEqualNumbers/SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<decimal> numbers = Console.ReadLine().Split().Select(decimal.Parse).ToList();
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    numbers[i] *= 2;
+                    numbers.Remove(numbers[i - 1]);
+                    i = 0;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
