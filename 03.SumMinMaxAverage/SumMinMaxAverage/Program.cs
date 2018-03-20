ca260ccfbbe01fa7720eba592148e1e4f7b58604
Radostin Ivanov <radipleven@abv.bg>
2018-03-20 17:17:15 +0200

    Add files via upload

---
+++ 03.SumMinMaxAverage/SumMinMaxAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SumMinMaxAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+            }
+
+            Console.WriteLine("Sum = " + numbers.Sum());
+            Console.WriteLine("Min = " + numbers.Min());
+            Console.WriteLine("Max = " + numbers.Max());
+            Console.WriteLine("Average = " + numbers.Average());
+        }
+    }
+}
